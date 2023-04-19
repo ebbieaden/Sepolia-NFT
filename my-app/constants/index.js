60f220314413ab3f7f1688b4270bc607c1a68d31
ebbieaden <47478940+ebbieaden@users.noreply.github.com>
2023-04-19 22:34:22 +0100

    Open sea nft api integrated

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
       "type": "receive"
     }
   ]
-export const NFT_CONTRACT_ADDRESS = "0x7439506C99b37Bd5edD869E094cf2b8d399d001a"
+export const NFT_CONTRACT_ADDRESS = "0xe88fe549D1BFf4A17C4ddc6e02cB7a9f5d7bf4a5"
